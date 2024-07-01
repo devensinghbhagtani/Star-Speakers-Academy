2a14e44b0236d22994df2852162a53f51da1812e
Manraj Singh Virdi <61323049+Manraj29@users.noreply.github.com>
2024-07-02 00:30:20 +0530

    Add files via upload
    To add this in current
    1.  ADD this in main.jsx
    
    import CourseHome from "./Course/CourseHome.jsx";
    import DisplayCourse from "./Course/displayCourse.jsx";
    
    then after Route1 
         <Route path="/course" element={<CourseHome />}> 
          <Route path="/course/displayCourse" element={<DisplayCourse />} />
          
          </Route>
    
    2. Alag se folder mai hi download karo as it is Course folder mai

---
+++ src/components/Course/CourseHome.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState } from 'react';
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import NavBar from './NavBar';
+import { BrowserRouter as Router, Route, BrowserRouter, Outlet } from 'react-router-dom';
+import Footer from './footer';
+
+export default function CourseHome() {
+
+    return (
+        <>
+        
+        <NavBar />
+        <br />
+        <br /><hr />
+        <br />
+        <Outlet />
+        <br />
+        <hr />
+        <Footer />
+        </>
+
+    )
+}
